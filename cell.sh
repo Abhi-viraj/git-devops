85284fccf32dd8749ffa3c418688bba78ad17af0
Ubuntu <ubuntu@ip-172-31-35-200.ap-southeast-2.compute.internal>
2026-03-29 10:06:57 +0000

    Trasfer the file in git repo.

---
+++ cell.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+#
+#Addtition of two number
+#Author: Abhinav Kunar
+############################
+#
+echo "Enter the number for Addition"
+
+echo "Enter the first number"
+read num1
+echo "Enter the 2nd number"
+read num2
+num3=$((num1+num2))
+
+echo "Total is $num3"
+
+echo "Enter the number for Multiplication"
+
+
+echo "Enter the first no."
+read num1
+echo "Enter the second number"
+read num2
+num3=$((num1 * num2))
+echo "Total is $num3"
+
+echo "Enter the number for Division"
+
+
+echo "Enter the first no."
+read num1
+echo "Enter the second  no."
+read num2
+num3=$((num1/num2))
+echo "Division is $num3"
+
+
+echo "Testing the braching method"
+
+echo "Enter the number for Subtraction"
+
+
+echo "Enter the first no."
+read num1
+echo "Enter the second  no."
+read num2
+num3=$((num1-num2))
+echo "Total is is $num3"
+
